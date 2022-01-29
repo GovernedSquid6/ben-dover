e6bf8ff29ebdada217898c1d8284d0e555211ca4
GovernedSquid6 <41247017+GovernedSquid6@users.noreply.github.com>
2022-01-28 18:28:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Intents } = require('discord.js');
+const allIntents = Discord.Intents.all();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: allIntents });
 
 client.on('ready', () => {
 	console.log('Rise and shine!');
