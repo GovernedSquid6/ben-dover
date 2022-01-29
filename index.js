72b1e6bce44b58ec2282e7de4d8744a972350a11
GovernedSquid6 <41247017+GovernedSquid6@users.noreply.github.com>
2022-01-29 01:44:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ client.on('messageCreate', async message => {
   if(command == "makesay") {
     var vic
     var vectim = args[0]
-    if (message.guild.members.fetch(vectim).catch(console.error)) {
+    var guildmems = await message.guild.members.fetch()
+    
+    if (guildmems.has(vectim)) {
       vic = await message.guild.members.fetch(vectim)
     }
     else {
