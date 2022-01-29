13524857895f01d1d97a6c68884c0d974b07906c
GovernedSquid6 <41247017+GovernedSquid6@users.noreply.github.com>
2022-01-28 19:34:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const allIntents = new Intents(32767);
 const client = new Client({ intents: allIntents });
 
 client.on('ready', () => {
-	console.log('Rise and shine!');
+  console.log('Rise and shine!');
 });
 
 client.on('messageCreate', message => {
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
   if(message.author.bot) return
-  if (message.content == "test") message.reply('pp');
+  if(command == "test") message.reply('pp');
+  if(command == "bensay") message.channel.send(args.join())
 });
 
 client.login(process.env.token);
