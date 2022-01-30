8b8eb5586fb289a1d1a189a53408f6bbe3490225
GovernedSquid6 <41247017+GovernedSquid6@users.noreply.github.com>
2022-01-29 18:46:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require('discord.js');
 const allIntents = new Intents(32767);
 
 const client = new Client({ intents: allIntents });
-var owner == "850097464123326515"
+var owner = "850097464123326515"
 
 client.on('ready', () => {
   console.log('Rise and shine!');
