0a973291b311b72c84cc2f128e68f4fdc4ed2811
GovernedSquid6 <41247017+GovernedSquid6@users.noreply.github.com>
2022-02-01 17:42:24 -0600

    Update makesay.js

--- commands/makesay.js
+++ commands/makesay.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('bensay')
+    .setDescription('Make Ben say something')
+    .addStringOption(option =>
+      option.setName('phrase')
+      .setDescription('What to make Ben say')
+      .setRequired(true));
+};
